650ea14823317fc025d881ee714d3d708cadd4c0
cocojgx <52567782+cocojgx@users.noreply.github.com>
2019-07-12 12:15:01 +0800

    Create User.java

---
+++ Grasshopper_server/src/com/hp/entity/User.java
@@ -0,0 +1,24 @@
+package com.hp.entity;
+
+public class User {
+private Integer id;
+
+public User() {
+	
+	// TODO Auto-generated constructor stub
+}
+
+public User(Integer id) {
+	super();
+	this.id = id;
+}
+
+public Integer getId() {
+	return id;
+}
+
+public void setId(Integer id) {
+	this.id = id;
+}
+
+}
