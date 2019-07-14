72f491747a69c4214d5d3ab31e213788e8947aef
pyf147 <52597653+pyf147@users.noreply.github.com>
2019-07-14 17:52:55 +0800

    Update QueryUserServices.java

--- Grasshopper_server/src/com/hp/services/QueryUserServices.java
+++ Grasshopper_server/src/com/hp/services/QueryUserServices.java
@@ -6,7 +6,7 @@ import com.hp.entity.Result;
 import com.hp.entity.TestUser;
 
 public interface QueryUserServices {
-	//查询用户是否存在或有无数据
+	//查询用户是否存在或有无数据,并且返回一个相当于的Result对象
 	public Result getData(String user_name);
 	//提供数据
 	public List<TestUser> getUser(String name);
