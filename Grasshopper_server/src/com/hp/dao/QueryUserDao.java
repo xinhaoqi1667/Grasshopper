a810bf2f63d95ee169793dc05ca10ff4818011eb
pyf147 <52597653+pyf147@users.noreply.github.com>
2019-07-17 14:55:00 +0800

    Update QueryUserDao.java

--- Grasshopper_server/src/com/hp/dao/QueryUserDao.java
+++ Grasshopper_server/src/com/hp/dao/QueryUserDao.java
@@ -2,13 +2,18 @@ package com.hp.dao;
 
 import java.util.List;
 
-import com.hp.entity.TestUser;
+import com.hp.entity.PersonalData;
+
+
 
 public class QueryUserDao extends BaseDao{
-	
+	public static void main(String[] args) {
+		QueryUserDao q = new QueryUserDao();
+		System.out.println(q.getUser("admin"));
+	}
 	//根据用户名查询用户个人相关信息
-	public List<TestUser> getUser(String name){
+	public List<PersonalData> getUser(String name){
 	  Object[] param = {name};
-	  return super.search("select user.name,college.college_name,major.major_name from user INNER JOIN college on user.college=college.id INNER JOIN major ON user.speciality=major.id WHERE user.name=?",TestUser.class, param);
+	  return super.search("SELECT user.id,user.name,college.college_name,major.major_name,user_message.grade FROM user_message INNER JOIN user ON user_message.user_id=user.id INNER JOIN college ON user_message.college=college.id INNER JOIN major ON user_message.speciality=major.id where user.name=?",PersonalData.class, param);
 	}
 }
