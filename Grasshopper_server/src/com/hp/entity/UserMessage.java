12648dad2b4f65cc98d32a00673bba9a8c20e838
pyf147 <52597653+pyf147@users.noreply.github.com>
2019-07-17 22:31:08 +0800

    Create UserMessage.java

---
+++ Grasshopper_server/src/com/hp/entity/UserMessage.java
@@ -0,0 +1,24 @@
+package com.hp.entity;
+
+public class UserMessage {
+private Integer id;
+
+public UserMessage() {
+	
+	// TODO Auto-generated constructor stub
+}
+
+public UserMessage(Integer id) {
+	super();
+	this.id = id;
+}
+
+public Integer getId() {
+	return id;
+}
+
+public void setId(Integer id) {
+	this.id = id;
+}
+
+}
