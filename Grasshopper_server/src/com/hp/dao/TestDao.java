3e0baa4c21af266fa46644d5b73d8d83a22a1c9a
pyf147 <52597653+pyf147@users.noreply.github.com>
2019-07-17 21:49:45 +0800

    Update TestDao.java

--- Grasshopper_server/src/com/hp/dao/TestDao.java
+++ Grasshopper_server/src/com/hp/dao/TestDao.java
@@ -1,13 +1,28 @@
 package com.hp.dao;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+
+import com.hp.entity.User;
+import com.hp.entity.test;
 
 
 
 //无用，测试上传空包用
 public class TestDao extends BaseDao{
-	
+	public static void main(String[] args) {
+		TestDao d = new TestDao();
+		List<test> user_ids = d.user_id(3);
+		List list = new ArrayList();
+		for (test user : user_ids) {
+			String user_name = d.user_name(user.getUser_id());
+			list.add(user_name);
+		}
+		for (Object object : list) {
+			System.out.println(object);
+		}
+	}
 	//测试（根据用户名查询密码是否存在）
 		public String findByName(String name)
 		{
@@ -19,4 +34,19 @@ public class TestDao extends BaseDao{
 			return password;
 			
 		}
+		
+		public List<test> user_id(int id){
+			Object[] params={id,id};
+			return super.QueryFocus("SELECT user_id FROM user_message where user_id !=? AND focus REGEXP ?",test.class, params);
+		}
+		
+		//根据id查询用户
+		public String user_name(int id){
+			Object[] params={id};
+			String name = super.QueryBy("select name from user where id=?","name",params);
+			return name;
+		}
+
 }
+
+
