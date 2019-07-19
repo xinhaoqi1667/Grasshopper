258185d0d93a2efa1ccac6dcd0782afa46fb40d3
pyf147 <52597653+pyf147@users.noreply.github.com>
2019-07-19 12:49:13 +0800

    Update FansDao.java

--- Grasshopper_server/src/com/hp/dao/FansDao.java
+++ Grasshopper_server/src/com/hp/dao/FansDao.java
@@ -6,11 +6,10 @@ import java.util.List;
 import com.hp.entity.Fans;
 
 public class FansDao extends BaseDao{
-	
 	//返回除了自己以外，在user_message的speciality(关注字段)中包含自己user_id的其他用户的user_id
 	public List<Fans> Fans_userid(int id){
 		Object[] params={id,id};
-		return super.QueryFocus("SELECT user_id FROM user_message where user_id !=? AND focus REGEXP ?",Fans.class, params);
+		return super.QueryList("SELECT user_id FROM user_message where user_id !=? AND focus REGEXP ?",Fans.class, params);
 	}
 	//根据id查询用户姓名,并返回一个集合
 	public List user_name(List<Fans> list){
