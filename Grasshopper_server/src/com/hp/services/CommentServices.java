ee7b29171b0a4b37093655cbdf9f1c44040ce86b
mrzhinice <52566759+mrzhinice@users.noreply.github.com>
2019-07-31 20:08:21 +0800

    完善了添加和查询方法

--- Grasshopper_server/src/com/hp/services/CommentServices.java
+++ Grasshopper_server/src/com/hp/services/CommentServices.java
@@ -7,5 +7,8 @@ import com.hp.entity.Comment;
 public interface CommentServices {//评论功能接口
 	public 	List<Comment> Queryfcomment(int rid);
 	public 	List<Comment> Querycomment(Comment com);
+	public 	List<Comment> Querybackcomment(Comment com);
 	public boolean addcoment(Comment com);
+	public boolean addfcoment(Comment com);
+	public boolean addffcoment(Comment com);
 }
